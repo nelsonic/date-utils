8db2b1354b1226c4bba65c107c7a638a833a329d
nelsonic <nodecoder@gmail.com>
2014-02-10 17:30:48 +0000

    adds dateRange method - 100% coverage

--- README.md
+++ README.md
@@ -3,34 +3,16 @@ rì
 
 A few useful (unit tested) JavaScript date methods
 
-### Background
-
-A comprehensive introduction to the JavaScript Date (*built-in methods*)
-is available on MDN:
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-
-### Existing (Popular) JS Date Libaries
-
-- Moment.js: http://momentjs.com/
-- Date.js: http://www.datejs.com/
-- Node Date Utils: https://github.com/JerrySievert/node-date-utils
-
-#### Other JS Related Libraries (Low Usage)
-
-- https://github.com/vesln/to-date (good tests)
-- https://github.com/coolaj86/javascript-date (rudimentary tests)
-- Ago: https://github.com/brycebaril/node-ago (simple methods, no tests!)
-- DT: https://github.com/peterbraden/dt.js (namespace squatting)
-
 
 ### Why?
 
 Why build *another* date utilities library when *several* already exist?
+(see list below)
 
-a) For fun. 
-b) Coding (JS TDD) practice
-c) Discovering interesting features
-d) only build/load what I need (*minimalist*)
+- For fun. 
+- Coding (JS TDD) practice
+- Discover interesting features
+- only build/load what I need (*minimalist*)
 
 ### Name (why call a date utilities library "ri"?)
 rì is the chinese word for date or day of the month.
@@ -45,3 +27,27 @@ In the interest of findability I'm considering changing the name of this repo
 to something more keyword friendly... Words:
 
 - Date, Utilities, Useful, Time, Helper
+
+## Background / Researh
+
+If you have never played with JavaScript **Date** try the
+comprehensive introduction to the JavaScript Date (*built-in methods*)
+on **M**ozilla **D**eveloper **N**etwork:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+
+### Existing (Popular) JS Date Libaries
+
+- Moment.js: http://momentjs.com/
+- Date.js: http://www.datejs.com/
+- Node Date Utils: https://github.com/JerrySievert/node-date-utils
+
+### Other JS Related Libraries (Low Usage)
+
+- Ago: https://github.com/brycebaril/node-ago 
+(two elegant methods, simple tests, zero promo === no traction)
+- To-date https://github.com/vesln/to-date 
+(good tests. *zero* watchers! strange... uses **ago** see above)
+- JS Date https://github.com/coolaj86/javascript-date (rudimentary tests)
+- DT: https://github.com/peterbraden/dt.js (namespace squatting)
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
 
-    test( "addLeadingZero adds a leading zero to a number less than 10", function() {
+    test( "addLeadingZero adds a leading zero to a number less than 10 (used in simpleDate)", function() {
         var input = 7;
         equal( DU.addLeadingZero(input), "07", true );
         var input = 2;
@@ -9,7 +9,6 @@
         var input = 0;
         equal( DU.addLeadingZero(input), "00", true );
     });
-
 
     test( "simpleDate returns the date as YYYY-MM-DD when you give it a Full JS Date object", function() {
 
@@ -25,21 +24,74 @@
 
     });
 
-
     test( "parseDate returns a full JS Date object for a simple date (YYYY-MM-DD)", function() {
         // exclude the time from expectation 
-        var now, simple, DD, MM, YYYY;
+        var now, simple, DD, MM, YYYY, nyd, nydSimple;
         now    = new Date();           // date to use below
         simple = DU.simpleDate(now);   // simple format YYYY-MM-DD (no time)
         DD     = now.getDate();
         MM     = now.getMonth();
         YYYY   = now.getFullYear();
         expect = new Date(YYYY, MM, DD);
-        console.log("Simple :", simple)
-        console.log("Expect :", expect.toString());
-        console.log("Got    :", DU.parseDate(simple).toString());
+        // console.log("Simple :", simple)
+        // console.log("Expect :", expect.toString());
+        // console.log("Got    :", DU.parseDate(simple).toString());
         // comparing two objects requires deepEqual: https://api.qunitjs.com/deepEqual/
         deepEqual( DU.parseDate(simple), expect, true );
+
+        // New Year's Day
+        nyd       = new Date(2014, 0, 1);
+        // console.log("New Year's Day: ",nyd.toString());
+        nydSimple = DU.simpleDate(nyd);
+        deepEqual( DU.parseDate(nydSimple), nyd, true );
     });
+
+    test( "addDayToDate adds a day to date in full JS or simple format", function() {
+        // New Year's Eve
+        var newYearsEve, nextDay, nextDaySimple;
+        newYearsEve = new Date(2014, 11, 31);
+        nextDay = DU.addDayToDate(newYearsEve);
+        nextDaySimple = DU.simpleDate(nextDay);
+        equal( nextDaySimple, "2015-01-01", true );
+
+    });
+
+
+    test( "addDayToDate leap year test 2016-02-28 + 1 = 2016-02-29", function() {
+        // New Year's Eve
+        var leapYearTest, nextDay, nextDaySimple;
+        // Leap Year Test expect nextDay to be 2016-02-29
+        leapYearTest = "2016-02-28";
+        nextDay = DU.addDayToDate(leapYearTest);
+        nextDaySimple = DU.simpleDate(nextDay);
+        equal( nextDaySimple, "2016-02-29", true );
+
+    });
+
+    test( "dateRange returns an array of dates between startDate and endDate", function() {
+        // New Year's Eve
+        var dateRange = ["2014-01-01", "2014-01-02", "2014-01-03", "2014-01-04"],
+            startDate = "2014-01-01", endDate = "2014-01-04";
+        deepEqual(DU.dateRange(startDate, endDate), dateRange, true);
+
+    });
+
+    test( "dateRange returns an array of dates without endDate", function() {
+        // New Year's Eve
+        var d, today, week, oneWeekAgo, dateRange = [];
+        today = new Date();
+        week  = 7 * 24 * 60 * 60 * 1000; 
+        oneWeekAgo = new Date(today.getTime() - week);
+        d = oneWeekAgo;
+        while(d < today) {
+            dateRange.push(DU.simpleDate(d));
+            d = DU.addDayToDate(d);
+        }
+        dateRange.push(DU.simpleDate(d));
+
+        deepEqual(DU.dateRange(DU.simpleDate(oneWeekAgo)), dateRange, true);
+
+    });
+
 
 }());
